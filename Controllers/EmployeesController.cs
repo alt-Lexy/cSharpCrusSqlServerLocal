8eed488798f0bb7327328eb638722ee802905af1
alt-Lexy <testpourledeveloppement@outlook.fr>
2024-12-13 10:59:48 +0100

    feat(api): init sql operation with sql server
    connexion with sql server locally  
    + api view with swagger UI

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeAdminPortal.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContextcs dbContext;
+        public EmployeesController(ApplicationDbContextcs dbContextcs)
+        {
+            this.dbContext = dbContextcs;
+            
+        }
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployees = dbContext.Employee.ToList();
+
+            return Ok(allEmployees);
+        }
+    }
+}
