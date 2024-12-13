332ae4cb6262418e829f19381822c17462c6f64e
alt-Lexy <testpourledeveloppement@outlook.fr>
2024-12-13 15:35:09 +0100

    feat(api): update and delete API

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -24,6 +24,16 @@ namespace EmployeeAdminPortal.Controllers
             return Ok(allEmployees);
         }
 
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeeById(Guid id) {
+            var employee = dbContext.Employee.Find(id);
+            if (employee is null) { 
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
         [HttpPost]
         public IActionResult AddEmployees(AddEmployeeDto addEmployeeDto)
         {
@@ -39,6 +49,41 @@ namespace EmployeeAdminPortal.Controllers
             dbContext.SaveChanges();
 
             return Ok(employeeEntity);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult updateEmployee(Guid id, UpdateEmployeeDto updateEmployeeDto)
+        {
+            var employeeId = dbContext.Employee.Find(id);
+            if (employeeId is null)
+            { 
+                return NotFound();
+            }
+
+            employeeId.Name = updateEmployeeDto.Name;
+            employeeId.Email = updateEmployeeDto.Email;
+            employeeId.Phone = updateEmployeeDto.Phone;
+            employeeId.Salary = updateEmployeeDto.Salary;
+
+            dbContext.SaveChanges();
+
+            return Ok(employeeId);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteEmployees(Guid id) 
+        { 
+            var employee = dbContext.Employee.Find(id);
+
+            if(employee is null)
+            {
+                return NotFound();
+            }
+
+            dbContext.Employee.Remove(employee);
+            dbContext.SaveChanges();
+            return Ok();
         }
     }
 }
